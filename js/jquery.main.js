8e0043e477a053ce09f797269843522608ad1c38
leraradinovich <lera.radinovich@webares.com.ua>
2016-02-16 15:28:07 +0200

    hero

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -8,6 +8,10 @@ $(function(){
             new Page ( $( this ) )
         } );
 
+        $.each($('.header-menu'), function () {
+            new Menu($(this));
+        });
+
     });
 
     var Page = function ( obj ) {
@@ -44,6 +48,132 @@ $(function(){
 
         _init();
 
+    };
+
+    var Menu = function (obj) {
+
+        //private properties
+        var _self = this,
+            _menu = obj,
+            _menuItems = _menu.find( '.header-menu__wrap>ul>li' ),
+            _subMenu = _menu.find( '.header-menu__subitems' ),
+            _window = $( window ),
+            _showBtn = $( '.menu-btn' );
+
+        //private methods
+        var _addEvents = function () {
+
+                _showBtn.on( {
+                    click: function () {
+                        var curItem = $( this );
+
+                        if(curItem.hasClass( 'opened' ) ) {
+
+                            curItem.removeClass( 'opened' );
+                            _menu.slideUp(300);
+
+                        } else {
+
+                            curItem.addClass( 'opened' );
+                            _menu.slideDown(300);
+
+                        }
+
+                    }
+                } );
+                _menuItems.find('a').on( {
+                    'click': function(){
+                        var curElem = $(this),
+                            subMenu = curElem.next( '.header-menu__subitems' );
+
+                        if(_window.width() < 992 && subMenu.length ) {
+
+                            if(curElem.hasClass( 'opened' ) ){
+
+                                curElem.removeClass('opened');
+                                subMenu.slideUp(300);
+
+                            } else {
+
+                                _subMenu.slideUp(300);
+                                _menuItems.find('a').removeClass('opened');
+
+                                curElem.addClass('opened');
+                                subMenu.slideDown(300);
+
+                            }
+
+                            return false;
+
+                        }
+
+                    }
+                } );
+                _menuItems.on( {
+                    'mouseenter': function() {
+
+                        var curElem = $(this).find('a'),
+                            subMenu = curElem.next( '.header-menu__subitems' );
+
+                        if( _window.width() >= 992 && subMenu.length ) {
+
+                            curElem.addClass( 'opened' )
+
+                        }
+
+                    },
+                    'mouseleave': function() {
+                        var curElem = $( this ).find( 'a' ),
+                            subMenu = curElem.next( '.header-menu__subitems' );
+
+                        if( _window.width() >= 992 && subMenu.length ){
+
+                            curElem.removeClass( 'opened' )
+
+                        }
+                    }
+                });
+                $(document).on(
+                    "click",
+                    ".header__menu>ul>li>ul",
+                    function( event ){
+                        event = event || window.event;
+
+                        if (event.stopPropagation) {
+                            event.stopPropagation();
+                        } else {
+                            event.cancelBubble = true;
+                        }
+                    }
+                );
+                $(document).on(
+                    "click",
+                    "body",
+                    function(){
+                        //_subMenu.slideUp(300);
+                        //_menuItems.find('a').removeClass('opened');
+                    }
+                );
+
+                _window.on({
+                    load: function () {
+                    },
+                    resize: function () {
+
+                        _showBtn.removeClass( 'opened' );
+                        _menuItems.find( 'a' ).removeClass( 'opened' );
+                        _menu.attr( 'style', '' );
+                        _subMenu.attr( 'style', '' );
+
+                    }
+                });
+
+            },
+            _init = function () {
+                _addEvents();
+            };
+
+        _init();
     };
 
 } );
