0a831189fc1a31dd839db9f596c673bbfc87c636
petruninevgeniy <petruninevgeniy@gmail.com>
2016-02-17 09:16:44 +0200

    created contacts

--- php/form.php
+++ php/form.php
@@ -2,13 +2,13 @@
     $json_data = str_replace("\r\n",'',$json_data);
     $json_data = str_replace("\n",'',$json_data);
 
-    $name = $_POST['name'];
-    $tel = $_POST['email'];
-    $text = $_POST['text'];
+    $name = $_GET['name'];
+    $tel = $_GET['email'];
+    $text = $_GET['text'];
 
 
     $headers  = "Content-type: text/html; charset=utf-8 \r\n";
-    $headers .= "From: adoric\r\n";
+    $headers .= "From: Fudge\r\n";
     $message = "Name: ".$name;
     $message .= ", Tel: ".$tel;
     $message .= ", Text: ".$text;
