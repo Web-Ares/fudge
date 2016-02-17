a619de8a7971fdee9ea8da9bbc7d420f734d648f
petruninevgeniy <petruninevgeniy@gmial.com>
2016-02-17 13:37:26 +0200

    hero

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -16,6 +16,13 @@ $(function(){
             new FormValidation ( $( this ) )
         } );
 
+        var swiper = new Swiper('.main-slider', {
+            paginationClickable: true,
+            nextButton: '.swiper-button-next',
+            prevButton: '.swiper-button-prev',
+            spaceBetween: 30
+        });
+
     });
 
     var Page = function ( obj ) {
@@ -24,6 +31,7 @@ $(function(){
             _obj = obj,
             _increase = _obj.find( '.site__increase' ),
             _footer = _obj.find( '.site__footer' ),
+            _header = $( '.site__header' ),
             _window = $( window );
 
         var _addEvents = function() {
@@ -36,6 +44,12 @@ $(function(){
 
                     resize: function(){
                         _calculateFooterHeight();
+                    },
+
+                    scroll: function() {
+
+                        _fixedHeader();
+
                     }
 
                 })
@@ -46,6 +60,19 @@ $(function(){
                 _increase.css({
                     height: _footer.innerHeight()
                 });
+
+            },
+            _fixedHeader = function() {
+
+                if( _window.scrollTop() > 0 ) {
+
+                    _header.addClass( 'fixed' );
+
+                } else {
+
+                    _header.removeClass( 'fixed' );
+
+                }
 
             },
             _init = function() {
@@ -60,104 +87,79 @@ $(function(){
 
     };
 
-    var Menu = function(obj) {
+    var Menu = function( obj ) {
 
         //private properties
         var _self = this,
             _menu = obj,
-            _menuItems = _menu.find('.header-menu__wrap>ul>li'),
-            _menuItemsLink = _menu.find('.header-menu__item'),
-            _subMenu = _menu.find('.header-menu__subitems'),
-            _window = $(window),
-            _showBtn = $('.menu-btn');
+            _menuItems = _menu.find( '.header-menu__dropdown' ),
+            _menuItemsLink = _menu.find( '.header-menu__item' ),
+            _subMenu = _menu.find( '.header-menu__subitems' ),
+            _window = $( window ),
+            _showBtn = $( '.menu-btn' );
 
         //private methods
         var _addEvents = function() {
 
                 _showBtn.on( {
-                    click: function () {
+                    'click': function() {
 
-                        _openMenu( $(this) );
+                        _openMenu( $( this ) );
 
                     }
                 } );
                 _menuItemsLink.on( {
                     'click': function(){
 
-                        _slideSubMenu( $(this) );
+                        _slideSubMenu( $( this ) );
 
                     }
                 } );
-                //_menuItems.on( {
-                //    'mouseenter': function() {
-                //
-                //        var curElem = $(this).find('a'),
-                //            subMenu = curElem.next( '.header-menu__subitems' );
-                //
-                //        if( _window.width() >= 992 && subMenu.length ) {
-                //
-                //            curElem.addClass( 'opened' );
-                //
-                //        }
-                //
-                //    },
-                //    'mouseleave': function() {
-                //        var curElem = $( this ).find( 'a' ),
-                //            subMenu = curElem.next( '.header-menu__subitems' );
-                //
-                //        if( _window.width() >= 992 && subMenu.length ){
-                //
-                //            curElem.removeClass( 'opened' );
-                //
-                //        }
-                //    }
-                //});
-
-                _window.on({
-                    resize: function () {
+                _window.on( {
+                    'resize': function() {
 
                         _resetStyle();
 
                     }
-                });
+                } );
 
             },
-            _openMenu = function(elem) {
+            _openMenu = function( elem )  {
 
                 var curItem = elem;
 
-                if(curItem.hasClass( 'opened' ) ) {
+                if( curItem.hasClass( 'opened' ) ) {
 
                     curItem.removeClass( 'opened' );
-                    _menu.slideUp(300);
+                    _menu.slideUp( 300 );
 
                 } else {
 
                     curItem.addClass( 'opened' );
-                    _menu.slideDown(300);
+                    _menu.slideDown( 300 );
 
                 }
 
             },
-            _slideSubMenu = function(elem) {
+            _slideSubMenu = function( elem ) {
 
                 var curElem = elem,
                     subMenu = curElem.next( '.header-menu__subitems' );
 
-                if(_window.width() < 992 && subMenu.length ) {
+                if( _window.width() < 992 && subMenu.length ) {
 
-                    if(curElem.hasClass( 'opened' ) ){
+                    if( curElem.hasClass( 'opened' ) ){
 
-                        curElem.removeClass('opened');
-                        subMenu.slideUp(300);
+                        curElem.removeClass( 'opened' );
+                        subMenu.slideUp( 300 );
 
                     } else {
 
-                        _subMenu.slideUp(300);
-                        _menuItems.find('a').removeClass('opened');
+                        _subMenu.slideUp( 300 );
+                        _menuItems.find( 'a' ).removeClass( 'opened' );
 
-                        curElem.addClass('opened');
-                        subMenu.slideDown(300);
+                        curElem.addClass( 'opened' );
+                        subMenu.slideDown( 300 );
 
                     }
 
@@ -175,7 +177,9 @@ $(function(){
 
             },
             _init = function() {
+
                 _addEvents();
+
             };
 
         _init();
