9d7e3454ede641366d30459aa88c99796457c12f
petruninevgeniy <petruninevgeniy@gmail.com>
2016-02-19 09:32:22 +0200

    created contacts

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -385,7 +385,30 @@ $(function(){
             _isGallery = false,
             _request = new XMLHttpRequest();
 
-        var _addEvents = function () {
+        var _addGalleryContent = function( msg ){
+
+                $.each( msg, function( i ){
+
+                    var newBlock = $( '<a href="' + this.href + '" title="' + this.title + '" class="gallery__item fancybox-group" data-fancybox-group="gallery" style="background-image: url(' + this.imageBG + ');"><span class="gallery__item-title">' + this.title + '</span></a>' );
+
+                    if ( i == 0 || i == 4 ){
+                        newBlock.addClass( 'gallery__item_height2x' );
+                    }
+
+                    if ( i == 2 || i == 4 ){
+                        newBlock.addClass( 'gallery__item_width2x' );
+                    }
+
+                    if ( this.video ){
+                        newBlock.addClass( 'gallery__item_video fancybox.iframe' );
+                    }
+
+                    _wrapper.append( newBlock );
+
+                } );
+
+            },
+            _addEvents = function () {
 
                 _window.on({
 
@@ -431,6 +454,7 @@ $(function(){
                 var galleryItem = _wrapper.find( '.gallery__item' );
 
                 _request.abort();
+
                 _request = $.ajax({
                     url: _btnAction,
                     data: {
@@ -440,7 +464,9 @@ $(function(){
                     timeout: 20000,
                     type: "GET",
                     success: function ( msg ) {
-                        console.log( msg );
+
+                        _addGalleryContent( msg );
+
                     },
                     error: function ( XMLHttpRequest ) {
                         if( XMLHttpRequest.statusText != "abort" ) {
@@ -453,6 +479,7 @@ $(function(){
             _destroyGallery = function(){
 
                 _wrapper.isotope( 'destroy' );
+
                 _isGallery = false;
 
             },
@@ -475,14 +502,17 @@ $(function(){
                         columnWidth: 0
                     }
                 });
+
                 _isGallery = true;
 
             },
             _initFancyBox = function(){
+
                 _fancyBoxGroup.fancybox({
                     closeBtn: false,
                     padding: [0,75,0,75]
                 });
+
             },
             _init = function () {
 
--- php/gallery-content.php
+++ php/gallery-content.php
@@ -4,73 +4,78 @@ $loadedCount = $_GET['loadedCount'];
 
 if ( $loadedCount == 9 ){
 
+//  "video" - if it is a block with a link to video frame, then - true. Else - false
+//  "title" - text which shows at hover on element
+//  "image-bg" - path to preview picture
+//  "href" - path to main picture, which shows in popup, or a link to the video
+
     $json_data = '[
 
                     {
                         "id":1,
                         "video": false,
                         "title": "Mouse hover",
-                        "image-bg": "pic/gallery__pic-1.jpg",
+                        "imageBG": "pic/gallery__pic-1.jpg",
                         "href": "pic/big/gallery__pic-1.jpg"
                         },
                     {
                         "id":2,
                         "video": false,
                         "title": "Mouse hover",
-                        "image-bg": "pic/gallery__pic-3.jpg",
+                        "imageBG": "pic/gallery__pic-3.jpg",
                         "href": "pic/big/gallery__pic-3.jpg"
                         },
                     {
                         "id":3,
                         "video": false,
                         "title": "Mouse hover",
-                        "image-bg": "pic/gallery__pic-5.jpg",
+                        "imageBG": "pic/gallery__pic-5.jpg",
                         "href": "pic/big/gallery__pic-5.jpg"
                         },
                     {
                         "id":4,
                         "video": false,
                         "title": "Mouse hover",
-                        "image-bg": "pic/gallery__pic-6.jpg",
+                        "imageBG": "pic/gallery__pic-6.jpg",
                         "href": "pic/big/gallery__pic-6.jpg"
                         },
                     {
                         "id":5,
                         "video": true,
                         "title": "Mouse hover",
-                        "image-bg": "pic/gallery__pic-4.jpg",
+                        "imageBG": "pic/gallery__pic-4.jpg",
                         "href": "https://www.youtube.com/embed/ymNFyxvIdaM"
                         },
                     {
                         "id":6,
                         "video": false,
                         "title": "Mouse hover",
-                        "image-bg": "pic/gallery__pic-7.jpg",
+                        "imageBG": "pic/gallery__pic-7.jpg",
                         "href": "pic/big/gallery__pic-7.jpg"
                         },
                     {
                         "id":7,
                         "video": false,
                         "title": "Mouse hover",
-                        "image-bg": "pic/gallery__pic-2.jpg",
+                        "imageBG": "pic/gallery__pic-2.jpg",
                         "href": "pic/big/gallery__pic-2.jpg"
                         },
                     {
                         "id":8,
                         "video": false,
                         "title": "Mouse hover",
-                        "image-bg": "pic/gallery__pic-8.jpg",
+                        "imageBG": "pic/gallery__pic-8.jpg",
                         "href": "pic/big/gallery__pic-8.jpg"
                         },
                     {
                         "id":9,
                         "video": false,
                         "title": "Mouse hover",
-                        "image-bg": "pic/gallery__pic-9.jpg",
+                        "imageBG": "pic/gallery__pic-9.jpg",
                         "href": "pic/big/gallery__pic-9.jpg"
                         }
 
-]';
+    ]';
 
 };
 
