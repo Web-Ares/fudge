223919c92e0e169162b3538a83ddcdeef41b08ee
petruninevgeniy <petruninevgeniy@gmial.com>
2016-02-22 13:23:36 +0200

    news

--- php/speakers-content.php
+++ php/speakers-content.php
@@ -2,14 +2,12 @@
 
 $loadedCount = $_GET['loadedCount'];
 
-if ( $loadedCount == 4 ){
-
 //  "has_items" - the number of not downloaded speakers
 //  "name" - name of speaker
 //  "post" - post of speaker
 //  "href" - link to speaker profile
 
-    $json_data = '{
+$json_data = '{
 
         "has_items": 1,
                     "items":[
@@ -50,36 +48,6 @@ if ( $loadedCount == 4 ){
                     ]
     }';
 
-} else {
-
-//  "has_items" - the number of not downloaded speakers
-//  "name" - name of speaker
-//  "post" - post of speaker
-//  "href" - link to speaker profile
-
-    $json_data = '{
-
-        "has_items": 0,
-            "items":[
-
-                        {
-                            "id":5,
-                            "picture": "pic/speaker-001.jpg",
-                            "name": "Jane ROTENBERG",
-                            "post": "Design Director and Co-founder of Stellar Artworks",
-                            "href": "#"
-                            },
-                        {
-                            "id":6,
-                            "picture": "pic/speaker-001.jpg",
-                            "name": "Jane ROTENBERG",
-                            "post": "Design Director and Co-founder of Stellar Artworks",
-                            "href": "#"
-                            }
-            ]
-        }';
-
-};
 echo $json_data;
 exit;
 ?>
