79b6cf3e99ed3066a12938d9ae00da0059c60a9f
iutin <evgeniy.iutin@gmail.com>
2016-02-23 14:47:53 +0200

    map balloon fix

--- php/location.php
+++ php/location.php
@@ -13,7 +13,7 @@ if (!empty($_GET['getlocations'])) {
                             "id":4,
                             "color":"#9b54d1",
                             "title": "Mouse hover",
-                            "icon": "/img/marker1.png",
+                            "icon": "img/marker1.png",
                             "coordinates": [51.504322, -0.376216],
                             "description": "some text1"
                             },
@@ -21,7 +21,7 @@ if (!empty($_GET['getlocations'])) {
                             "id":5,
                             "color":"#345cc2",
                             "title": "Mouse hover2",
-                            "icon": "/img/marker1.png",
+                            "icon": "img/marker2.png",
                             "coordinates": [51.514322, -0.576216],
                             "description": "some text2"
                             },
@@ -29,7 +29,7 @@ if (!empty($_GET['getlocations'])) {
                             "id":6,
                             "color":"#ff8e42",
                             "title": "Mouse hover3",
-                            "icon": "/img/marker1.png",
+                            "icon": "img/marker1.png",
                             "coordinates": [51.524322, -0.676216],
                             "description": "some text3"
                             },
@@ -37,7 +37,7 @@ if (!empty($_GET['getlocations'])) {
                             "id":7,
                             "color":"#9b54d1",
                             "title": "Mouse hover4",
-                            "icon": "/img/marker1.png",
+                            "icon": "img/marker3.png",
                             "coordinates": [51.534322, -0.976216],
                             "description": "some text4"
                             },
@@ -45,7 +45,7 @@ if (!empty($_GET['getlocations'])) {
                             "id":8,
                             "color":"#345cc2",
                             "title": "Mouse hover5",
-                            "icon": "/img/marker1.png",
+                            "icon": "img/marker1.png",
                             "coordinates": [51.544322, -0.176216],
                             "description": "some text5"
                             },
@@ -53,7 +53,7 @@ if (!empty($_GET['getlocations'])) {
                             "id":9,
                             "color":"#ff8e42",
                             "title": "Mouse hover",
-                            "icon": "/img/marker1.png",
+                            "icon": "img/marker1.png",
                             "coordinates": [51.544322, -0.976216],
                             "description": "some text"
                             }
